5381b50988bdaead18babad1db404aa6b32c76d5
topiy <topiyjorge@gmail.com>
2019-06-02 05:47:02 +0200

    Subida con los cambios Test

---
+++ src/test/java/com/example/Information/personTest/FindAll.java
@@ -0,0 +1,39 @@
+package com.example.Information.personTest;
+
+import com.example.Information.models.PersonModel;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class FindAll {
+
+	@Autowired
+	public TestRestTemplate testRestTemplate;
+
+
+	@Test
+	public void givenValidTerm_shouldSuccessWith200AndReturnsList(){
+		UriComponents url = UriComponentsBuilder.fromUriString("/people").build();
+		ResponseEntity<List<PersonModel>> result =
+				testRestTemplate.exchange("/people", HttpMethod.GET,
+										  new HttpEntity<>(new HttpHeaders()),
+										  new ParameterizedTypeReference<List<PersonModel>>() {});
+
+		assertEquals(HttpStatus.OK, result.getStatusCode());
+		assertNotNull(result.getBody());
+	}
+}
---
+++ src/test/java/com/example/Information/personTest/Save.java
@@ -0,0 +1,38 @@
+package com.example.Information.personTest;
+
+import com.example.Information.models.PersonModel;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class Save {
+
+	@Autowired
+	public TestRestTemplate testRestTemplate;
+
+
+	@Test
+	public void givenValidTerm_shouldSuccessWith200AndReturnsOneObject(){
+		UriComponents url = UriComponentsBuilder.fromUriString("/people/{id}").build();
+		PersonModel personModel = new PersonModel();
+		ResponseEntity<PersonModel> result =
+				testRestTemplate.exchange("/people/{id}", HttpMethod.GET,
+										  new HttpEntity<>(new HttpHeaders()),
+										  new ParameterizedTypeReference<PersonModel>() {});
+
+		assertEquals(HttpStatus.OK, result.getStatusCode());
+		assertNotNull(result.getBody());
+	}
+}
